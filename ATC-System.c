40deb4023faa994844da728f1837ef2cf5cf2212
MannShah0312 <134050095+MannShah0312@users.noreply.github.com>
2024-02-28 14:52:27 +0000

    ATC.c updated

--- ATC-System.c
+++ ATC-System.c
@@ -3,12 +3,24 @@
 #include <string.h>
 typedef enum {am, pm} AntePost;
 
-typedef struct TIME_NODE {
+typedef struct Time_Node {
     int hr;
     int min;
     AntePost meridiem;
-} TIME;
+} Time;
+
+typedef struct FlightPlan_Node {
+    int flightID;
+    Time depart;
+    Time Arrival;
+    struct FlightPlan_Node *next;
+} Flight_Plan;
 
 typedef struct Bucket_Node {
-    
+    Time Beg_ETA;
+    Time End_ETA;
+    int BucketID;
+    Flight_Plan *FlightList;
+    struct Bucket_Node *next;
 } Bucket;
+
