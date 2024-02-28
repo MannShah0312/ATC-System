7109d547f2540e002ff23564f5808a181d3371e4
MannShah0312 <134050095+MannShah0312@users.noreply.github.com>
2024-02-28 14:26:42 +0000

    ATC modified

--- ATC-System.c
+++ ATC-System.c
@@ -1,4 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+typedef enum {am, pm} AntePost;
 
+typedef struct TIME_NODE {
+    int hr;
+    int min;
+    AntePost meridiem;
+} TIME;
+
+typedef struct Bucket_Node {
+    
+} Bucket;
