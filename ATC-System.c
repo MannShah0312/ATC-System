ba933c86d8bbb00ecc591f5ac745f22efaca0be2
MannShah0312 <134050095+MannShah0312@users.noreply.github.com>
2024-02-28 15:29:37 +0000

    ATC updated

--- ATC-System.c
+++ ATC-System.c
@@ -12,15 +12,55 @@ typedef struct Time_Node {
 typedef struct FlightPlan_Node {
     int flightID;
     Time depart;
-    Time Arrival;
+    Time arrival;
     struct FlightPlan_Node *next;
 } Flight_Plan;
 
 typedef struct Bucket_Node {
-    Time Beg_ETA;
-    Time End_ETA;
+    Time ETA_Beg;
+    Time ETA_End;
     int BucketID;
     Flight_Plan *FlightList;
     struct Bucket_Node *next;
 } Bucket;
 
+Time makeTime(int hr, int min, AntePost meridiem) {
+    Time t;
+
+    t.hr = hr;
+    t.min = min;
+    t.meridiem = meridiem;
+
+    return t;
+}
+
+Bucket *makeBucketNode(int id, Time Beg) {
+    Bucket *newB = (Bucket *)malloc(sizeof(Bucket));
+
+    newB->BucketID = id;
+    newB->ETA_Beg = Beg;
+    newB->ETA_End = Beg;
+    newB->ETA_End.min = 59;
+    newB->FlightList = NULL;
+    newB->next = NULL;
+
+    return newB;
+}
+
+Flight_Plan *makeNewFlightPlan(int id, Time dep, Time arr) {
+    Flight_Plan *new = (Flight_Plan *)malloc(sizeof(Flight_Plan));
+
+    new->flightID = id;
+    new->depart = dep;
+    new->arrival = arr;
+    new->next = NULL;
+
+    return new;
+}
+
+void addNewFlightPlan(Bucket **dashboard, Flight_Plan *newFP) {
+    Bucket *curr = *dashboard;
+    Bucket *prev;
+
+    while (curr && curr) {}
+}
